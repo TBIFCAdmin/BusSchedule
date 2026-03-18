d2dadb3e1dde2ec4d6316b760b6c5243f90433b3
TBIFCAdmin <david.paterson@tbifc.ca>
2026-03-18 14:26:19 -0400

    Update BusSchedule.py

--- BusSchedule.py
+++ BusSchedule.py
@@ -14,26 +14,30 @@ options = Options()
 options.add_argument("--headless=new")
 
 driver = webdriver.Chrome(options)
-wait = WebDriverWait(driver,60)
+wait = WebDriverWait(driver,15)
 driver.get("https://nextlift.ca/#f=Clavet%20%26%20Cumberland%20(1128)")
 
 driver2= webdriver.Chrome(options)
-wait2 = WebDriverWait(driver2,60)
+wait2 = WebDriverWait(driver2,15)
 driver2.get("https://nextlift.ca/#f=Clavet%20%26%20Cumberland%20(1180)")
-st.write("<style> .responsive-text {font-size: 2vw; padding: 10px;} .block-container {padding-top: 2rem; padding-bottom: 0rem; padding-left: 5rem; padding-right: 5rem;} table {width: 100%; border: none; background-color: #003360;} th {border: none; font-size: 32px} table td {border: none; font-size: 30px} td:first-child {background-color: rgb(191, 79, 157); color: rgb(255, 255, 255)}</style>", unsafe_allow_html=True)
+st.write('<b><style> .block-container {padding-top: 5rem; padding-bottom: 0rem; padding-left: 5rem; padding-right: 5rem;} table {width: 100%; border: none; background-color: #003360;} th {border: none; font-size: 44px} table td {border: none; font-size: 36px} td:first-child {background-color: rgb(191, 79, 157); color: rgb(255, 255, 255)}</style></b>', unsafe_allow_html=True)
 
-@st.fragment(run_every="15s")
+
+@st.fragment(run_every="60s")
 def Get_Page_Warning(local_driver, local_wait):
     try:
         dynamic_element = local_wait.until(EC.presence_of_element_located((By.ID, "noResults")))
         warnings = local_driver.find_elements(By.ID, "noResults")
         for x in warnings:
             if x.text != "":
-                st.markdown(f'<div class="responsive-text" style="font-weight: bold; color:orange;">{x.text}</div>', unsafe_allow_html=True)
+                st.markdown(f'<p style="font-weight: bold; font-size:36px; color:orange;">{x.text}</p>', unsafe_allow_html=True)
     except TimeoutException:
         pass
         
-@st.fragment(run_every="15s")
+    
+
+
+@st.fragment(run_every="60s")
 def Get_Page_Info(local_driver, local_wait):
     try:
         dynamic_element = local_wait.until(EC.presence_of_element_located((By.CLASS_NAME, "header-result")))
@@ -44,7 +48,7 @@ def Get_Page_Info(local_driver, local_wait):
     if txtHeader == "":
         txtHeader = "No header Result pulled"
 
-    st.markdown(f'<p style="font-weight: bold; font-size:30px;">{txtHeader}</p>', unsafe_allow_html=True)
+    st.markdown(f'<p style="font-weight: bold; font-size:36px;">{txtHeader}</p>', unsafe_allow_html=True)
     list_of_lists =[]
     try:
         dynamic_element = local_wait.until(EC.presence_of_element_located((By.CLASS_NAME, "header-main")))
@@ -68,9 +72,13 @@ def Get_Page_Info(local_driver, local_wait):
         list_of_lists,
         headers=["Route", "Destination", "Arrival"],
         tablefmt="html"
-    )                    
+    )
+                     
+
+
     st.markdown(table, unsafe_allow_html=True)
-  
+
+    
 Get_Page_Info(driver,wait)
 Get_Page_Info(driver2,wait2)
 Get_Page_Warning(driver,wait)
